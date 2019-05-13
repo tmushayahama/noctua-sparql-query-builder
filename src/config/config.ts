5519132fb260d4b052657e2e0470eb03d90c66ed
tmushayahama <tmushayahama@gmail.com>
2019-05-13 11:06:37 -0700

    init with sparql-query-builder as template

---
+++ src/config/config.ts
@@ -0,0 +1,15 @@
+export const INDENT_SIZE = 4
+
+export const PREFIX: any = {
+    rdf: '<http://www.w3.org/1999/02/22-rdf-syntax-ns#>',
+    rdfs: '<http://www.w3.org/2000/01/rdf-schema#>',
+    dc: '<http://purl.org/dc/elements/1.1/>',
+    metago: '<http://model.geneontology.org/>',
+    owl: '<http://www.w3.org/2002/07/owl#>',
+    GO: '<http://purl.obolibrary.org/obo/GO_>',
+    BP: '<http://purl.obolibrary.org/obo/GO_0008150>',
+    MF: '<http://purl.obolibrary.org/obo/GO_0003674>',
+    CC: '<http://purl.obolibrary.org/obo/GO_0005575>',
+    providedBy: '<http://purl.org/pav/providedBy>',
+    has_affiliation: '<http://purl.obolibrary.org/obo/ERO_0000066>'
+}
